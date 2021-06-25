f9d854bfe7d009cbd1aa6a6f4c177707d7633ea9
krishpranav <krisna.pranav@gmail.com>
2021-06-25 14:46:44 +0530

    [+] Adding files [+]

---
+++ JailBrokenFinder/JailBroken.m
@@ -0,0 +1,9 @@
+//
+//  JailBroken.m
+//  JailBrokenFinder
+//
+//  Created by Elangovan Ayyasamy on 25/06/21.
+//  Copyright © 2021 Krisna Pranav. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
