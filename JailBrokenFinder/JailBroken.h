28d02fa3592f918c827df0968f63b84d3eea144e
krishpranav <krisna.pranav@gmail.com>
2021-06-25 14:58:37 +0530

    [+] imports [+]

--- JailBrokenFinder/JailBroken.h
+++ JailBrokenFinder/JailBroken.h
@@ -6,8 +6,23 @@
 //  Copyright © 2021 Krisna Pranav. All rights reserved.
 //
 
-#ifndef JailBroken_h
-#define JailBroken_h
+#import <Foundation/Foundation.h>
+#import <sys/stat.h>
+#import <UIKit/UIKit.h>
+#include <mach-o/dyld.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/sysctl.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <dlfcn.h>
+#include "TargetConditionals.h"
 
 
-#endif /* JailBroken_h */
+
