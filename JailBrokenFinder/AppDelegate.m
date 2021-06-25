e7a349a0b47980e7a0655dac8718a9d7f98dd613
krishpranav <krisna.pranav@gmail.com>
2021-06-25 14:53:15 +0530

    [+] App Delegate [+]

--- JailBrokenFinder/AppDelegate.m
+++ JailBrokenFinder/AppDelegate.m
@@ -14,12 +14,34 @@
 
 @implementation AppDelegate
 
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     return YES;
 }
 
+
 - (void)applicationWillResignActive:(UIApplication *)application {
+}
+
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
 
 }
+
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+
+}
+
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+
+}
+
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+
+}
+
 
 @end
